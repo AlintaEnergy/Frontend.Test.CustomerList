6090ada1f3f389fc09f8f61254ec758b5ee9d6e0
YLFxGen <lyk1224476255@gmail.com>
2023-07-22 17:22:07 +1000

    add validation

--- src/components/AddCustomer/AddCustomerForm.tsx
+++ src/components/AddCustomer/AddCustomerForm.tsx
@@ -1,61 +1,99 @@
-import { Field, Formik, FormikHelpers } from "formik";
-import * as React from "react";
-import { ICustomer, Customer } from "../../types/types";
+import { Field, Formik, FormikHelpers } from 'formik';
+import * as React from 'react';
+import { ICustomer, Customer } from '../../types/types';
+import * as Yup from 'yup';
 import {
-  StyledForm,
-  StyledInput,
-  StyledLabel,
-  StyledAddButton,
-} from "./StyledAddCustomerForm";
+    StyledForm,
+    StyledInput,
+    StyledLabel,
+    StyledAddButton,
+    StyledErrorMessage,
+} from './StyledAddCustomerForm';
+
+const AddCustomerSchema = Yup.object().shape({
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    phoneNumber: Yup.string()
+        .matches(/^(?:\+61|0)[1-9](?:[0-9]?[0-9])?[0-9]{6}$/, {
+            message: 'Invalid phone number',
+            excludeEmptyString: true,
+        })
+        .required('Required'),
+});
 
 type Props = {
-  saveCustomer: (customer: ICustomer | any) => void;
+    saveCustomer: (customer: ICustomer | any) => void;
 };
 
 export const AddCustomerForm: React.FC<Props> = ({ saveCustomer }) => {
-  return (
-    <Formik
-      initialValues={{
-        firstName: "",
-        lastName: "",
-        phoneNumber: "",
-      }}
-      onSubmit={(
-        values: Customer,
-        { setSubmitting }: FormikHelpers<Customer>
-      ) => {
-        saveCustomer(values);
-        setSubmitting(false);
-      }}
-    >
-      <StyledForm>
-        <StyledLabel htmlFor="firstName">First Name</StyledLabel>
-        <Field
-          as={StyledInput}
-          id="firstName"
-          name="firstName"
-          placeholder="John"
-        />
+    return (
+        <Formik
+            initialValues={{
+                firstName: '',
+                lastName: '',
+                phoneNumber: '',
+            }}
+            validationSchema={AddCustomerSchema}
+            onSubmit={(
+                values: Customer,
+                { setSubmitting }: FormikHelpers<Customer>
+            ) => {
+                saveCustomer(values);
+                setSubmitting(false);
+            }}
+        >
+            {({ errors, touched }) => (
+                <StyledForm>
+                    <StyledLabel htmlFor='firstName'>First Name</StyledLabel>
+                    <Field
+                        as={StyledInput}
+                        id='firstName'
+                        name='firstName'
+                        placeholder='John'
+                    />
 
-        <StyledLabel htmlFor="lastName">Last Name</StyledLabel>
-        <Field
-          as={StyledInput}
-          id="lastName"
-          name="lastName"
-          placeholder="Doe"
-        />
+                    {errors.firstName && touched.firstName ? (
+                        <StyledErrorMessage>
+                            {errors.firstName}
+                        </StyledErrorMessage>
+                    ) : null}
 
-        <StyledLabel htmlFor="phoneNumber">Phone Number</StyledLabel>
-        <Field
-          as={StyledInput}
-          id="phoneNumber"
-          name="phoneNumber"
-          placeholder="john@acme.com"
-          type="tel"
-        />
+                    <StyledLabel htmlFor='lastName'>Last Name</StyledLabel>
+                    <Field
+                        as={StyledInput}
+                        id='lastName'
+                        name='lastName'
+                        placeholder='Doe'
+                    />
 
-        <StyledAddButton type="submit">Add Customer</StyledAddButton>
-      </StyledForm>
-    </Formik>
-  );
+                    {errors.lastName && touched.lastName ? (
+                        <StyledErrorMessage>
+                            {errors.lastName}
+                        </StyledErrorMessage>
+                    ) : null}
+
+                    <StyledLabel htmlFor='phoneNumber'>
+                        Phone Number
+                    </StyledLabel>
+                    <Field
+                        as={StyledInput}
+                        id='phoneNumber'
+                        name='phoneNumber'
+                        placeholder='john@acme.com'
+                        type='tel'
+                    />
+
+                    {errors.phoneNumber && touched.phoneNumber ? (
+                        <StyledErrorMessage>
+                            {errors.phoneNumber}
+                        </StyledErrorMessage>
+                    ) : null}
+
+                    <StyledAddButton type='submit'>
+                        Add Customer
+                    </StyledAddButton>
+                </StyledForm>
+            )}
+        </Formik>
+    );
 };
